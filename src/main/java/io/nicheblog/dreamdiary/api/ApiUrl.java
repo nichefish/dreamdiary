ba22ffb50d917d6b1b2156ff134506680011f71d
nichefish <nichefish@gmail.com>
2024-03-21 03:19:10 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/api/ApiUrl.java
@@ -0,0 +1,16 @@
+package io.nicheblog.dreamdiary.api;
+
+/**
+ * ApiUrl
+ * <pre>
+ *  API:: 공통 상수:: API 호출 URL 정의
+ * </pre>
+ *
+ * @author nichefish
+ */
+public interface ApiUrl {
+
+    String PREFIX_API = "/api";
+
+    
+}
---
+++ src/main/java/io/nicheblog/dreamdiary/web/WebSiteMenu.java
@@ -0,0 +1,17 @@
+package io.nicheblog.dreamdiary.web;
+
+import io.nicheblog.dreamdiary.api.ApiUrl;
+
+/**
+ * SiteMenu
+ * <pre>
+ *  공통 상수 :: 사이트 메뉴 번호, 이름, URL 정의
+ * </pre>
+ *
+ * @author nichefish
+ * @implements ApiUrl
+ */
+public interface WebSiteMenu {
+
+
+}
