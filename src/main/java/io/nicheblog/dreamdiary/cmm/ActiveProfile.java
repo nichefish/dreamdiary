f4f879d8857f2d395aa13813183ba22f0b9d3736
nichefish <nichefish@gmail.com>
2024-03-21 03:36:40 +0900

    activeProfile 추가

---
+++ src/main/java/io/nicheblog/dreamdiary/cmm/ActiveProfile.java
@@ -0,0 +1,75 @@
+package io.nicheblog.dreamdiary.cmm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * ActiveProfile
+ * <pre>
+ *  현재 활성 중인 프로필 정보를 가져온다. (local / dev / stg / prod)
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Component("activeProfile")
+@ConfigurationProperties(prefix = "spring.profiles")
+@Getter
+@Setter
+public final class ActiveProfile {
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Profile {
+        LOCAL("local", "로컬 환경"),
+        DEV("dev", "개발 환경"),
+        STG("stg", "스테이징 환경"),
+        PROD("prod", "운영 환경"),
+        TEST("test", "테스트 환경");
+
+        private final String key;
+        private final String desc;
+    }
+
+    private Profile profile;
+
+    private final String PROFILE_LOCAL = Profile.LOCAL.getKey();
+    private final String PROFILE_DEV = Profile.DEV.getKey();
+    private final String PROFILE_STG = Profile.STG.getKey();
+    private final String PROFILE_PROD = Profile.PROD.getKey();
+    private final String PROFILE_TEST = Profile.TEST.getKey();
+
+    /**
+     * 활성화된 프로필 항목
+     */
+    private String active;
+
+    /* ----- */
+
+    public Boolean isLocal() {
+        return PROFILE_LOCAL.equals(this.active);
+    }
+
+    public Boolean isDev() {
+        return PROFILE_DEV.equals(this.active);
+    }
+
+    public Boolean isStg() {
+        return PROFILE_STG.equals(this.active);
+    }
+
+    public Boolean isProd() {
+        return PROFILE_PROD.equals(this.active);
+    }
+
+    public Boolean isNotProd() {
+        return !this.isProd();
+    }
+
+    public Boolean isTest() {
+        return PROFILE_TEST.equals(this.active);
+    }
+}
