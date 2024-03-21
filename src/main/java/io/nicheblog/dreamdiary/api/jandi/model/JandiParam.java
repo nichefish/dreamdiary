d054e2d4741a772dc51ff7bfb227b71343ab717f
nichefish <nichefish@gmail.com>
2024-03-21 21:14:50 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/api/jandi/model/JandiParam.java
@@ -0,0 +1,36 @@
+package io.nicheblog.dreamdiary.api.jandi.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * JandiParam
+ * <pre>
+ *  API:: JANDI:: JANDI API 호출 파라미터 Dto
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@SuperBuilder
+public class JandiParam {
+
+    /**
+     * 대상 토픽 코드
+     */
+    @Schema(description = "대상 토픽 코드", example = "NOTICE")
+    private String trgetTopic;
+
+    /**
+     * 메세지
+     */
+    @Schema(description = "대상 토픽 코드", example = "새로운 휴가계획서가 등록되었습니다.")
+    private String msg;
+}
