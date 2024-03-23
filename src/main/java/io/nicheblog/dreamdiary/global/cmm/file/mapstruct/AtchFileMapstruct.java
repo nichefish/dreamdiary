0a1a0a23c9ccebd4d7c20347296fc53f71179f1f
nichefish <nichefish@gmail.com>
2024-03-24 03:31:43 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/global/cmm/file/mapstruct/AtchFileMapstruct.java
+++ src/main/java/io/nicheblog/dreamdiary/global/cmm/file/mapstruct/AtchFileMapstruct.java
@@ -1,13 +1,3 @@
-/*
- * Copyright 2000-2022 BixSoft Co., All Rights Reserved.
- *   http://www.bixsoft.net
- *
- * This source code is digitised property of BixSoft Company Limited ("BixSoft") and
- * protected by copyright under the law of Republic of Korea and other foreign laws.
- * Reproduction and/or redistribution of the source code without written permission of
- * BixSoft is not allowed. Also, it is severely prohibited to register whole or specific
- * part of the source code to any sort of information retrieval system.
- */
 package io.nicheblog.dreamdiary.global.cmm.file.mapstruct;
 
 import io.nicheblog.dreamdiary.global.cmm.file.entity.AtchFileEntity;
