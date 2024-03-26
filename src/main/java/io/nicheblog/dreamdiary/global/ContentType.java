dbb729cec6c5afc80080fe1a2913eaadb5ad3838
nichefish <nichefish@gmail.com>
2024-03-27 04:56:56 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/global/ContentType.java
@@ -0,0 +1,28 @@
+package io.nicheblog.dreamdiary.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * ContentType
+ * <pre>
+ *  ClsfEntity를 상속받은 클래스들이 사용하는 컨텐츠 타입 정보
+ * </pre>
+ *
+ * @author nichefish
+ */
+@RequiredArgsConstructor
+@Getter
+public enum ContentType {
+
+    NOTICE("notice", "공지사항"),
+    COMMENT("comment", "댓글"),
+    DREAM_DAY("dream_day", "꿈 일자"),
+    DREAM_PIECE("dream_piece", "꿈 조각"),;
+
+    public final String key;
+    public final String desc;
+}
