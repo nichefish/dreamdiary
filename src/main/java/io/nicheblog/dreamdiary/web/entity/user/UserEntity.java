7521d515fb8469f56d0e5cc9c6f7662435c90b1d
nichefish <nichefish@gmail.com>
2024-03-27 05:54:51 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/entity/user/UserEntity.java
+++ src/main/java/io/nicheblog/dreamdiary/web/entity/user/UserEntity.java
@@ -1,8 +1,6 @@
 package io.nicheblog.dreamdiary.web.entity.user;
 
-import io.nicheblog.dreamdiary.global.Constant;
-import io.nicheblog.dreamdiary.global.cmm.cd.entity.DtlCdEntity;
-import io.nicheblog.dreamdiary.global.intrfc.entity.BaseUserEntity;
+import io.nicheblog.dreamdiary.global.intrfc.entity.BaseAtchEntity;
 import io.nicheblog.dreamdiary.global.util.DateUtils;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -16,7 +14,6 @@ import javax.persistence.Entity;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -31,110 +28,69 @@ import java.util.List;
  * @extends BaseUserEntity
  */
 @Entity
-@Table(name = "USER")
+@Table(name = "user")
 @DynamicInsert      // null인 값은 (null로 insert하는 대신) insert에서 제외
 @Getter
 @Setter
 @SuperBuilder(toBuilder=true)
 @AllArgsConstructor
 @RequiredArgsConstructor
-@ToString(exclude = {"userInfo"}, callSuper = true)
-@Where(clause = "DEL_YN='N'")
-@SQLDelete(sql = "UPDATE USER SET DEL_YN = 'Y' WHERE USER_NO = ?")
+@Where(clause = "del_yn='N'")
+@SQLDelete(sql = "UPDATE user SET del_yn = 'Y' WHERE user_no = ?")
 public class UserEntity
-        extends BaseUserEntity {
+        extends BaseAtchEntity {
 
     /**
      * 사용자 번호 (PK)
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "USER_NO")
+    @Column(name = "user_no")
     @Comment("사용자 번호")
     private Integer userNo;
 
     /**
-     * 사용자 정보 번호 (위임)
-     */
-    @Column(name = "USER_INFO_NO")
-    @Comment("사용자 정보 번호")
-    private Integer userInfoNo;
-
-    /**
-     * 사용자 정보
-     */
-    @OneToOne
-    @JoinColumn(name = "USER_INFO_NO", referencedColumnName = "USER_INFO_NO", insertable = false, updatable = false)
-    @Fetch(FetchMode.SELECT)
-    @NotFound(action = NotFoundAction.IGNORE)
-    @Comment("사용자 정보")
-    private UserInfoEntity userInfo;
-
-    /**
      * 사용자 아이디
      */
-    @Column(name = "USER_ID", length = 20, unique = true)
+    @Column(name = "user_id", length = 20, unique = true)
     @Comment("사용자 아이디")
     private String userId;
 
     /**
-     * 비밀번호
+     * 사용자 프로필 정보
      */
-    // 암호화된 비밀번호(64bit)를 저장하기 위해 길이=64이다.
-    @Column(name = "USER_PW", length = 64)
-    @Comment("비밀번호")
-    private String userPw;
+    @OneToOne(mappedBy = "user")
+    @NotFound(action = NotFoundAction.IGNORE)
+    @Comment("사용자 프로필 정보")
+    private UserProflEntity userProfl;
 
     /**
-     * 표시이름 : 사용자 정보가 없을때 표시되는 이름
+     * 비밀번호 :: 암호화된 비밀번호(64bit)를 저장하기 위해 길이=64이다.
      */
-    @Column(name = "NICK_NM", length = 50)
+    @Column(name = "password", length = 64)
+    @Comment("비밀번호")
+    private String password;
+
+    /**
+     * 표시이름 : 사용자 프로필 정보가 없을 때 표시되는 이름
+     */
+    @Column(name = "nick_nm", length = 50)
     @Comment("표시이름")
     private String nickNm;
 
     /**
-     * 권한코드
-     */
-    @Column(name = "AUTH_CD", length = 20)
-    @Comment("권한코드")
-    private String authCd;
-
-    /**
-     * 권한코드 정보 (복합키 조인)
-     */
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumnsOrFormulas({
-            @JoinColumnOrFormula(formula = @JoinFormula(value = "\'" + Constant.AUTH_CD + "\'", referencedColumnName = "CL_CD")),
-            @JoinColumnOrFormula(column = @JoinColumn(name = "AUTH_CD", referencedColumnName = "DTL_CD", insertable = false, updatable = false))
-    })
-    @Fetch(value = FetchMode.JOIN)
-    @NotFound(action = NotFoundAction.IGNORE)
-    @Comment("권한코드 정보")
-    private DtlCdEntity authCdInfo;
-
-    /**
-     * 권한 정보
-     */
-    @ManyToOne
-    @JoinColumn(name = "AUTH_CD", referencedColumnName = "AUTH_CD", insertable = false, updatable = false)
-    @Fetch(value = FetchMode.JOIN)
-    @NotFound(action = NotFoundAction.IGNORE)
-    @Comment("권한 정보")
-    private UserRoleEntity roleInfo;
-
-    /**
-     * 접속 IP 사용여부
+     * 접속 IP 사용 여부
      */
     @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "ACS_IP_YN", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    @Comment("접속 IP 사용여부")
-    private String acsIpYn = "N";
+    @Column(name = "use_acs_ip_yn", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
+    @Comment("접속 IP 사용 여부")
+    private String useAcsIpYn = "N";
 
     /**
      * 접속 IP 정보
      */
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    @JoinColumn(name = "USER_NO")
+    @JoinColumn(name = "user_no")
     @Fetch(FetchMode.SELECT)
     @OrderBy("acsIp ASC")
     @NotFound(action = NotFoundAction.IGNORE)
@@ -142,82 +98,24 @@ public class UserEntity
     private List<UserAcsIpEntity> acsIpInfoList;
 
     /**
-     * 마지막 로그인 일시
-     */
-    @DateTimeFormat(pattern = DateUtils.PTN_DATETIME)
-    @Column(name = "LST_LGN_DT")
-    @Comment("마지막 로그인 일시")
-    private Date lstLgnDt;
-
-    /**
-     * 로그인 실패 횟수
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "LGN_FAIL_CNT", columnDefinition = "INT DEFAULT 0")
-    @Comment("로그인 실패 횟수")
-    private Integer lgnFailCnt = 0;
-
-    /**
-     * 패스워드 변경일시
-     */
-    @DateTimeFormat(pattern = DateUtils.PTN_DATETIME)
-    @Column(name = "PW_CHG_DT")
-    @Comment("패스워드 변경일시")
-    private Date pwChgDt;
-
-    /**
-     * 퇴사여부
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "RETIRE_YN", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    @Comment("퇴사여부")
-    private String retireYn = "N";
-
-    /**
      * 계정 설명 (관리자용)
      */
-    @Column(name = "USER_DC", length = 1000)
+    @Column(name = "user_dc", length = 1000)
     @Comment("계정 설명")
     private String userDc;
 
     /**
-     * 패스워드 리셋 필요여부
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "NEEDS_PW_RESET", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    @Comment("패스워드 리셋 필요여부")
-    private String needsPwReset = "N";
-
-    /**
-     * 장기 미로그인 패스 체크 여부
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "DORMANT_BYPASS_YN", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    @Comment("장기 미로그인 패스 체크 여부")
-    private String dormantBypassYn = "N";
-
-    /**
      * 프로필 이미지 URL
      */
-    @Column(name = "PROFL_IMG_URL", length = 1000)
+    @Column(name = "profl_img_url", length = 1000)
     @Comment("프로필 이미지 URL")
     private String proflImgUrl;
 
     /**
-     * 본인신청여부
+     * 계정 상태 정보 (위임)
      */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "REQST_YN", length = 1, columnDefinition = "CHAR DEFAULT 'Y'")
-    @Comment("본인신청여부")
-    private String reqstYn = "Y";
-
-    /**
-     * 승인여부
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "CF_YN", length = 1, columnDefinition = "CHAR DEFAULT 'Y'")
-    @Comment("승인여부")
-    private String cfYn = "Y";
+    @Embedded
+    public UserStusInfo acntStus;
 
     /* ----- */
 
---
+++ src/main/java/io/nicheblog/dreamdiary/web/entity/user/UserStusInfo.java
@@ -0,0 +1,83 @@
+package io.nicheblog.dreamdiary.web.entity.user;
+
+import io.nicheblog.dreamdiary.global.util.DateUtils;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Comment;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Date;
+
+/**
+ * UserStusInfo
+ * <pre>
+ *  사용자user에서 계정 상태 관련 정보 분리
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Embeddable
+@Getter
+@Setter
+public class UserStusInfo {
+
+    /**
+     * 잠금여부
+     */
+    @Column(name = "locked_yn", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
+    protected String lockedYn = "N";
+
+    /**
+     * 마지막 로그인 일시
+     */
+    @DateTimeFormat(pattern = DateUtils.PTN_DATETIME)
+    @Column(name = "lst_lgn_dt")
+    @Comment("마지막 로그인 일시")
+    private Date lstLgnDt;
+
+    /**
+     * 로그인 실패 횟수
+     */
+    @Column(name = "lgn_fail_cnt", columnDefinition = "INT DEFAULT 0")
+    @Comment("로그인 실패 횟수")
+    private Integer lgnFailCnt = 0;
+
+    /**
+     * 패스워드 변경일시
+     */
+    @DateTimeFormat(pattern = DateUtils.PTN_DATETIME)
+    @Column(name = "pw_chg_dt")
+    @Comment("패스워드 변경일시")
+    private Date pwChgDt;
+
+    /**
+     * 패스워드 리셋 필요여부
+     */
+    @Column(name = "needs_pw_reset", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
+    @Comment("패스워드 리셋 필요여부")
+    private String needsPwReset = "N";
+
+    /**
+     * 장기 미로그인 패스 체크 여부
+     */
+    @Column(name = "dormant_bypass_yn", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
+    @Comment("장기 미로그인 패스 체크 여부")
+    private String dormantBypassYn = "N";
+
+    /**
+     * 본인신청여부
+     */
+    @Column(name = "reqst_yn", length = 1, columnDefinition = "CHAR DEFAULT 'Y'")
+    @Comment("본인신청여부")
+    private String reqstYn = "Y";
+
+    /**
+     * 승인여부
+     */
+    @Column(name = "cf_yn", length = 1, columnDefinition = "CHAR DEFAULT 'Y'")
+    @Comment("승인여부")
+    private String cfYn = "Y";
+}
