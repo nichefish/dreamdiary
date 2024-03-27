6b61f3878d90fbb258619ee2fd2165589419951a
nichefish <nichefish@gmail.com>
2024-03-27 13:05:34 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/DreamdiaryApplication.java
+++ src/main/java/io/nicheblog/dreamdiary/DreamdiaryApplication.java
@@ -6,8 +6,13 @@ import io.nicheblog.dreamdiary.global.Constant;
 import io.nicheblog.dreamdiary.global.cmm.log.event.LogSysEvent;
 import io.nicheblog.dreamdiary.global.cmm.log.model.LogSysParam;
 import io.nicheblog.dreamdiary.global.util.MessageUtils;
+import io.nicheblog.dreamdiary.web.entity.admin.LgnPolicyEntity;
 import io.nicheblog.dreamdiary.web.entity.user.UserEntity;
+import io.nicheblog.dreamdiary.web.entity.user.UserStusInfo;
+import io.nicheblog.dreamdiary.web.model.admin.LgnPolicyDto;
+import io.nicheblog.dreamdiary.web.repository.admin.LgnPolicyRepository;
 import io.nicheblog.dreamdiary.web.repository.user.UserRepository;
+import io.nicheblog.dreamdiary.web.service.admin.LgnPolicyService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -38,11 +43,8 @@ public class DreamdiaryApplication
     @Resource(name = "activeProfile")
     private ActiveProfile activeProfile;
 
-    @Resource(name = "passwordEncoder")
-    private PasswordEncoder passwordEncoder;
-
-    @Resource(name = "userRepository")
-    private UserRepository userRepository;
+    @Resource(name = "dreamdiaryInitializer")
+    private DreamdiaryInitializer initializer;
 
     @Resource
     protected ApplicationEventPublisher publisher;
@@ -62,64 +64,14 @@ public class DreamdiaryApplication
 
         log.info("activeProfile: {}", activeProfile.getActive());
 
-        // 시스템계정 등록
-        this.chckSystemAcnt();
+        // 시스템 계정 부재시 등록
+        initializer.chkSystemAcnt();
+        // 로그인 정책 부재시 등록
+        initializer.chkLgnPolicy();
 
+        // 시스템 재기동 로그 적재
         if (!activeProfile.isProd()) return;
-
         LogSysParam logParam = new LogSysParam(true, "시스템이 정상적으로 재기동되었습니다.");
         publisher.publishEvent(new LogSysEvent(this, logParam));
-    }
-
-    /**
-     * 사용자가 공백이므로 최초 실행시 관리자 자동 등록 (PW 암호화)
-     */
-    public void chckSystemAcnt() {
-
-        LogSysParam logParam = new LogSysParam();
-
-        boolean isSuccess = false;
-        boolean systemAcntExists = false;
-        String resultMsg = "";
-        try {
-            // 시스템계정 존재여부 체크
-            Optional<UserEntity> rsSystemAccountInfo = userRepository.findByUserId(Constant.SYSTEM_ACNT);
-            if (rsSystemAccountInfo.isPresent()) {
-                systemAcntExists = true;
-                return;
-            }
-            // 시스템계정 등록:: 메소드 분리
-            isSuccess = this.regSystemAcnt();
-            resultMsg = MessageUtils.getMessage(isSuccess ? MessageUtils.RSLT_SUCCESS : MessageUtils.RSLT_FAILURE);
-        } catch (Exception e) {
-            isSuccess = false;
-            resultMsg = MessageUtils.getExceptionMsg(e);
-            logParam.setExceptionInfo(MessageUtils.getExceptionNm(e), e.getMessage());
-        } finally {
-            if (!systemAcntExists) {
-                logParam.setResult(isSuccess, resultMsg);
-                publisher.publishEvent(new LogSysEvent(this, logParam));
-            }
-        }
-
-    }
-
-    /**
-     * 시스템 계정 등록:: 메소드 분리
-     * 임의의 고정 패스워드로 생성되었으므로 최초설치 후 직접 비밀번호를 변경해 주어야 한다.
-     */
-    public boolean regSystemAcnt() {
-        final String initTempPw = "123qwe!QA";
-
-        final UserEntity system = UserEntity.builder()
-                .userNo(1)
-                .authCd(Constant.AUTH_MNGR)
-                .nickNm(Constant.SYSTEM_ACNT_NM)
-                .userId(Constant.SYSTEM_ACNT)
-                .userPw(passwordEncoder.encode(initTempPw))
-                .regstrId(Constant.SYSTEM_ACNT)
-                .build();
-        UserEntity rsltEntity = userRepository.saveAndFlush(system);
-        return (rsltEntity.getUserNo() != null);
     }
 }
---
+++ src/main/java/io/nicheblog/dreamdiary/DreamdiaryInitializer.java
@@ -0,0 +1,141 @@
+package io.nicheblog.dreamdiary;
+
+import io.nicheblog.dreamdiary.global.Constant;
+import io.nicheblog.dreamdiary.global.cmm.log.event.LogSysEvent;
+import io.nicheblog.dreamdiary.global.cmm.log.model.LogSysParam;
+import io.nicheblog.dreamdiary.global.util.MessageUtils;
+import io.nicheblog.dreamdiary.web.entity.user.UserEntity;
+import io.nicheblog.dreamdiary.web.model.admin.LgnPolicyDto;
+import io.nicheblog.dreamdiary.web.model.user.UserDto;
+import io.nicheblog.dreamdiary.web.service.admin.LgnPolicyService;
+import io.nicheblog.dreamdiary.web.service.user.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+
+/**
+ * DreamdiaryInitializer
+ * <pre>
+ *  어플리케이션 초기화 로직 분리 클래스
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Component("dreamdiaryInitializer")
+@Log4j2
+public class DreamdiaryInitializer {
+
+    @Resource(name = "passwordEncoder")
+    private PasswordEncoder passwordEncoder;
+
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @Resource(name = "lgnPolicyService")
+    private LgnPolicyService lgnPolicyService;
+
+    @Resource
+    protected ApplicationEventPublisher publisher;
+
+    public final String INIT_TEMP_PW = "123qwe!QA";
+
+    /**
+     * 최초 실행시 사용자가 공백이므로 관리자 계정 자동 등록 (PW 암호화)
+     */
+    public void chkSystemAcnt() {
+
+        LogSysParam logParam = new LogSysParam();
+
+        boolean isSuccess = false;
+        boolean systemAcntExists = false;
+        String resultMsg = "";
+        try {
+            // 시스템계정 존재여부 체크
+            UserDto rsSystemAccountInfo = userService.getDtlDto(Constant.SYSTEM_ACNT);
+            if (rsSystemAccountInfo != null) {
+                systemAcntExists = true;
+                return;
+            }
+            // 시스템계정 등록:: 메소드 분리
+            isSuccess = this.regSystemAcnt();
+            resultMsg = MessageUtils.getMessage(isSuccess ? MessageUtils.RSLT_SUCCESS : MessageUtils.RSLT_FAILURE);
+        } catch (Exception e) {
+            isSuccess = false;
+            resultMsg = MessageUtils.getExceptionMsg(e);
+            logParam.setExceptionInfo(MessageUtils.getExceptionNm(e), e.getMessage());
+        } finally {
+            if (!systemAcntExists) {
+                logParam.setResult(isSuccess, resultMsg);
+                publisher.publishEvent(new LogSysEvent(this, logParam));
+            }
+        }
+    }
+
+    /**
+     * 시스템 계정 등록:: 메소드 분리
+     * 임의의 고정 패스워드로 생성되었으므로 최초설치 후 직접 비밀번호를 변경해 주어야 한다.
+     */
+    public boolean regSystemAcnt() throws Exception {
+        final UserDto systemAcnt = UserDto.builder()
+                .nickNm(Constant.SYSTEM_ACNT_NM)
+                .userId(Constant.SYSTEM_ACNT)
+                .password(passwordEncoder.encode(INIT_TEMP_PW))
+                .regstrId(Constant.SYSTEM_ACNT)
+                .build();
+
+        UserDto rslt = userService.regist(systemAcnt);
+        return (rslt.getUserNo() != null);
+    }
+
+    /**
+     * 최초 실행시 로그인 정책이 공백이므로 기본값 자동 등록 (PW 암호화)
+     */
+    public void chkLgnPolicy() {
+
+        LogSysParam logParam = new LogSysParam();
+
+        boolean isSuccess = false;
+        boolean systemAcntExists = false;
+        String resultMsg = "";
+        try {
+            // 로그인 정책 존재여부 체크
+            LgnPolicyDto rsLgnPolicy = lgnPolicyService.getDtlDto();
+            if (rsLgnPolicy != null) {
+                systemAcntExists = true;
+                return;
+            }
+            // 로그인 정책 등록:: 메소드 분리
+            isSuccess = this.regLgnPolicy();
+            resultMsg = MessageUtils.getMessage(isSuccess ? MessageUtils.RSLT_SUCCESS : MessageUtils.RSLT_FAILURE);
+        } catch (Exception e) {
+            isSuccess = false;
+            resultMsg = MessageUtils.getExceptionMsg(e);
+            logParam.setExceptionInfo(MessageUtils.getExceptionNm(e), e.getMessage());
+        } finally {
+            if (!systemAcntExists) {
+                logParam.setResult(isSuccess, resultMsg);
+                publisher.publishEvent(new LogSysEvent(this, logParam));
+            }
+        }
+    }
+
+    /**
+     * 로그인 정책 등록:: 메소드 분리
+     * 임의의 고정 패스워드로 생성되었으므로 최초설치 후 직접 비밀번호를 변경해 주어야 한다.
+     */
+    public boolean regLgnPolicy() throws Exception {
+
+        final LgnPolicyDto lgnPolicy = LgnPolicyDto.builder()
+                .lgnTryLmt(5)
+                .pwChgDy(90)
+                .lgnLockDy(90)
+                .pwForReset(INIT_TEMP_PW)
+                .build();
+
+        return lgnPolicyService.regist(lgnPolicy);
+    }
+}
