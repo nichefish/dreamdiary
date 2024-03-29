61f12a11d5c88b17d1d35906d80f78eecb70fcf5
nichefish <nichefish@gmail.com>
2024-03-30 07:15:52 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/web/mapstruct/vcatn/stats/VcatnStatsMapstruct.java
@@ -0,0 +1,75 @@
+package io.nicheblog.dreamdiary.web.mapstruct.vcatn.stats;
+
+import io.nicheblog.dreamdiary.global.intrfc.mapstruct.BaseMapstruct;
+import io.nicheblog.dreamdiary.global.util.DateUtils;
+import io.nicheblog.dreamdiary.web.entity.vcatn.stats.VcatnStatsEntity;
+import io.nicheblog.dreamdiary.web.model.vcatn.stats.VcatnStatsDto;
+import io.nicheblog.dreamdiary.web.model.vcatn.stats.VcatnStatsListXlsxDto;
+import org.apache.commons.lang3.StringUtils;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * VcatnStatsMapstruct
+ * <pre>
+ *  휴가 통계 MapStruct 기반 Mapper 인터페이스
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, imports = {DateUtils.class, StringUtils.class})
+public interface VcatnStatsMapstruct
+        extends BaseMapstruct<VcatnStatsDto, VcatnStatsEntity> {
+
+    VcatnStatsMapstruct INSTANCE = Mappers.getMapper(VcatnStatsMapstruct.class);
+
+    /**
+     * Entity -> Dto
+     */
+    @Mapping(target = "cnwkYy", expression = "java(entity.getCnwkYy() != null ? entity.getCnwkYy() : 0)")
+    @Mapping(target = "bsYryc", expression = "java(entity.getBsYryc() != null ? entity.getBsYryc() : 0)")
+    @Mapping(target = "cnwkYryc", expression = "java(entity.getCnwkYryc() != null ? entity.getCnwkYryc() : 0)")
+    @Mapping(target = "prjctYryc", expression = "java(entity.getPrjctYryc() != null ? entity.getPrjctYryc() : 0)")
+    @Mapping(target = "refreshYryc", expression = "java(entity.getRefreshYryc() != null ? entity.getRefreshYryc() : 0)")
+    VcatnStatsDto toDto(final VcatnStatsEntity entity) throws Exception;
+
+    /**
+     * StatsDto -> StatsXlsxDto
+     */
+    @Mapping(target = "userNm", expression = "java(dto.getUser() != null ? dto.getUser().getUserNm() : \"\")")
+    @Mapping(target = "ecnyDt", expression = "java(dto.getUser() != null ? dto.getUser().getEcnyDt() : \"\")")
+    @Mapping(target = "cnwkYy", expression = "java(dto.getCnwkYy() != null ? Integer.toString(dto.getCnwkYy()) : \"0\")")
+    @Mapping(target = "bsYryc", expression = "java(dto.getBsYryc() != null ? Integer.toString(dto.getBsYryc()) : \"0\")")
+    @Mapping(target = "cnwkYryc", expression = "java(dto.getCnwkYryc() != null ? Integer.toString(dto.getCnwkYryc()) : \"0\")")
+    @Mapping(target = "prjctYryc", expression = "java(dto.getPrjctYryc() != null ? Integer.toString(dto.getPrjctYryc()) : \"0\")")
+    @Mapping(target = "refreshYryc", expression = "java(dto.getRefreshYryc() != null ? Integer.toString(dto.getRefreshYryc()) : \"0\")")
+    @Mapping(target = "totYryc", expression = "java(Double.toString(dto.getTotYryc()))")
+    @Mapping(target = "exhsYryc", expression = "java(Double.toString(dto.getExhsYryc()))")
+    @Mapping(target = "remndrYryc", expression = "java(Double.toString(dto.getRemndrYryc()))")
+    VcatnStatsListXlsxDto toListXlsxDto(final VcatnStatsDto dto) throws Exception;
+
+    /**
+     * Dto -> Entity
+     */
+    @Mapping(target = "cnwkYy", expression = "java(dto.getCnwkYy() != null ? dto.getCnwkYy() : 0)")
+    @Mapping(target = "bsYryc", expression = "java(dto.getBsYryc() != null ? dto.getBsYryc() : 0)")
+    @Mapping(target = "cnwkYryc", expression = "java(dto.getCnwkYryc() != null ? dto.getCnwkYryc() : 0)")
+    @Mapping(target = "prjctYryc", expression = "java(dto.getPrjctYryc() != null ? dto.getPrjctYryc() : 0)")
+    @Mapping(target = "refreshYryc", expression = "java(dto.getRefreshYryc() != null ? dto.getRefreshYryc() : 0)")
+    VcatnStatsEntity toEntity(final VcatnStatsDto dto) throws Exception;
+
+    /**
+     * update Entity from Dto (Dto에서 null이 아닌 값만 Entity로 매핑)
+     */
+    @Override
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "cnwkYy", expression = "java(dto.getCnwkYy() != null ? dto.getCnwkYy() : 0)")
+    @Mapping(target = "bsYryc", expression = "java(dto.getBsYryc() != null ? dto.getBsYryc() : 0)")
+    @Mapping(target = "cnwkYryc", expression = "java(dto.getCnwkYryc() != null ? dto.getCnwkYryc() : 0)")
+    @Mapping(target = "prjctYryc", expression = "java(dto.getPrjctYryc() != null ? dto.getPrjctYryc() : 0)")
+    @Mapping(target = "refreshYryc", expression = "java(dto.getRefreshYryc() != null ? dto.getRefreshYryc() : 0)")
+    void updateFromDto(
+            final VcatnStatsDto dto,
+            final @MappingTarget VcatnStatsEntity entity
+    ) throws Exception;
+}
