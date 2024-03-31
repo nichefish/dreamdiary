e03860e0615a7be52e639f603bb08b24f1d223b2
nichefish <nichefish@gmail.com>
2024-03-31 14:03:43 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/web/service/cmm/viewer/ViewerService.java
@@ -0,0 +1,54 @@
+package io.nicheblog.dreamdiary.web.service.cmm.viewer;
+
+import io.nicheblog.dreamdiary.global.auth.util.AuthUtils;
+import io.nicheblog.dreamdiary.global.intrfc.entity.BaseClsfKey;
+import io.nicheblog.dreamdiary.web.entity.cmm.viewer.ViewerEntity;
+import io.nicheblog.dreamdiary.web.repository.cmm.viewer.ViewerRepository;
+import io.nicheblog.dreamdiary.web.spec.cmm.viewer.ViewerSpec;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.map.HashedMap;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * ViewerService
+ * <pre>
+ *  컨텐츠  게시물 열람자 서비스 모듈
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Service("ViewerService")
+@Log4j2
+public class ViewerService {
+
+    @Resource(name = "viewerRepository")
+    private ViewerRepository viewerRepository;
+    @Resource(name = "viewerSpec")
+    private ViewerSpec viewerSpec;
+
+    /**
+     * 게시물 열람자 존재여부 (기 방문여부) 체크
+     */
+    public Boolean hasAlreadyVisited(final BaseClsfKey key) {
+        Map<String, Object> searchParamMap = new HashedMap<>() {{
+            put("userId", AuthUtils.getLgnUserId());
+            put("postNo", key.getPostNo());
+            put("contentType", key.getContentType());
+        }};
+        Page<ViewerEntity> viewerList = viewerRepository.findAll(viewerSpec.searchWith(searchParamMap), Pageable.unpaged());
+        return viewerList.getSize() > 0;
+    }
+
+    /**
+     * 게시물 열람자 등록
+     */
+    public void addViewer(final BaseClsfKey key) {
+        if (this.hasAlreadyVisited(key)) return;
+        viewerRepository.save(new ViewerEntity(key));
+    }
+}
