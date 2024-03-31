c0021364907f0aede12904b1113460f78059b60f
nichefish <nichefish@gmail.com>
2024-03-31 20:26:17 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/repository/cmm/managtr/ManagtrRepository.java
+++ src/main/java/io/nicheblog/dreamdiary/web/repository/cmm/managtr/ManagtrRepository.java
@@ -1,24 +1,20 @@
-/*
-package io.nicheblog.dreamdiary.web.repository.board;
+package io.nicheblog.dreamdiary.web.repository.cmm.managtr;
 
-import dreamdiary.nicheblog.io.cmm.intrfc.repository.BaseRepository;
-import dreamdiary.nicheblog.io.web.entity.BasePostKey;
-import dreamdiary.nicheblog.io.web.entity.board.BoardPostManagtrEntity;
+import io.nicheblog.dreamdiary.global.intrfc.repository.BaseRepository;
+import io.nicheblog.dreamdiary.web.entity.cmm.managt.ManagtrEntity;
 import org.springframework.stereotype.Repository;
 
-*/
 /**
- * BoardPostManagtrRepository
+ * ManagtrRepository
  * <pre>
- *  게시판 게시물 작업자 Repository 인터페이스
+ *  일반게시판 게시물 작업자 Repository 인터페이스
  *  ※게시물 게시물 작업자(board_post_managtr) = 게시판 수정이력자. 게시판 게시물(board_post)에 1:N으로 귀속된다.
  * </pre>
  *
  * @author nichefish
- *//*
+ */
+@Repository("managtrRepository")
+public interface ManagtrRepository
+        extends BaseRepository<ManagtrEntity, Integer> {
 
-@Repository("boardPostManagtrRepository")
-public interface BoardPostManagtrRepository
-        extends BaseRepository<BoardPostManagtrEntity, BasePostKey> {
-
-}*/
+}
