f06bdbbe1549b6c04512127b143962e1e6134943
nichefish <nichefish@gmail.com>
2024-04-05 07:41:51 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/entity/user/UserAuthRoleEntity.java
+++ src/main/java/io/nicheblog/dreamdiary/web/entity/user/UserAuthRoleEntity.java
@@ -1,6 +1,7 @@
 package io.nicheblog.dreamdiary.web.entity.user;
 
-import io.nicheblog.dreamdiary.global.auth.entity.AuthRole;
+import io.nicheblog.dreamdiary.global.auth.entity.AuthRoleEntity;
+import io.nicheblog.dreamdiary.global.intrfc.entity.BaseCrudEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.*;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 /**
  * UserAuthRoleEntity
  * <pre>
- *  사용자 권한 Entity
+ *  사용자-권한 Entity
  * </pre>
  *
  * @author nichefish
@@ -22,54 +23,39 @@ import javax.persistence.*;
 @DynamicInsert      // null인 값은 (null로 insert하는 대신) insert에서 제외
 @Getter
 @Setter
-@SuperBuilder(toBuilder=true)
+@SuperBuilder(toBuilder = true)
 @RequiredArgsConstructor
 @AllArgsConstructor
 @ToString(callSuper = true)
 @Where(clause = "del_yn='N'")
 @SQLDelete(sql = "UPDATE user_auth_role SET del_yn = 'Y' WHERE user_auth_role_no = ?")
-public class UserAuthRoleEntity {
+public class UserAuthRoleEntity
+        extends BaseCrudEntity {
 
-    /**
-     * 사용자 권한 번호
-     */
+    /** 사용자-권한 번호 (PK) */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_auth_role_no")
-    @Comment("사용자 권한 번호")
+    @Comment("사용자 권한 번호 (PK)")
     private Integer userAuthRoleNo;
 
-    /**
-     * 사용자 정보
-     */
+    /** 사용자 정보 */
     @ManyToOne
     @JoinColumn(name = "user_no", referencedColumnName = "user_no", insertable = false, updatable = false)
     @NotFound(action = NotFoundAction.IGNORE)
     @Comment("사용자 정보")
     private UserEntity user;
 
-    /**
-     * 권한 코드
-     */
+    /** 권한 코드 */
     @Column(name = "auth_cd")
     @Comment("권한 코드")
     private String authCd;
 
-    /**
-     * 권한 정보 매핑
-     */
+    /** 권한 정보 매핑 */
     @ManyToOne
     @JoinColumn(name = "auth_cd", referencedColumnName = "auth_cd", insertable = false, updatable = false)
     @Fetch(value = FetchMode.JOIN)
     @NotFound(action = NotFoundAction.IGNORE)
     @Comment("작업자 정보")
-    private AuthRole roleInfo;
-
-    /**
-     * 삭제 여부
-     */
-    @Builder.Default        // Builder 사용시 초기값 세팅하도록 설정
-    @Column(name = "del_yn", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    @Comment("삭제 여부")
-    private String delYn = "N";
+    private AuthRoleEntity roleInfo;
 }
--- src/main/java/io/nicheblog/dreamdiary/web/model/user/UserAuthRoleDto.java
+++ src/main/java/io/nicheblog/dreamdiary/web/model/user/UserAuthRoleDto.java
@@ -1,23 +1,17 @@
 package io.nicheblog.dreamdiary.web.model.user;
 
-import io.nicheblog.dreamdiary.global.auth.entity.AuthRole;
+import io.nicheblog.dreamdiary.global.auth.entity.AuthRoleEntity;
 import io.nicheblog.dreamdiary.global.intrfc.model.BaseAtchDto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.Comment;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
- * UserDto
+ * UserAuthRoleDto
  * <pre>
- *  사용자(계정) 정보 Dto
+ *  사용자-권한 Dto
  * </pre>
  *
  * @author nichefish
@@ -25,28 +19,16 @@ import javax.persistence.Id;
  */
 @Getter
 @Setter
-@SuperBuilder(toBuilder=true)
+@SuperBuilder(toBuilder = true)
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class UserAuthRoleDto
         extends BaseAtchDto {
 
-    /**
-     * 사용자 권한 번호
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_auth_role_no")
-    @Comment("사용자 권한 번호")
+    /** 사용자 권한 번호 (PK) */
     private Integer userAuthRoleNo;
-
-    /**
-     * 권한 코드
-     */
+    /** 권한 코드 */
     private String authCd;
-
-    /**
-     * 권한 정보 매핑
-     */
-    private AuthRole role;
+    /** 권한 정보 매핑 */
+    private AuthRoleEntity role;
 }
