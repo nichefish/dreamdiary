e25385ad864d1be2f31cd18ff9fd17c438b5f156
nichefish <nichefish@gmail.com>
2024-04-05 20:14:05 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/spec/admin/TmplatDefSpec.java
+++ src/main/java/io/nicheblog/dreamdiary/web/spec/admin/TmplatDefSpec.java
@@ -1,6 +1,7 @@
 package io.nicheblog.dreamdiary.web.spec.admin;
 
-import io.nicheblog.dreamdiary.global.intrfc.spec.BaseSpec;
+import io.nicheblog.dreamdiary.global.intrfc.spec.BaseCrudSpec;
+import io.nicheblog.dreamdiary.global.intrfc.spec.embed.BaseStateSpec;
 import io.nicheblog.dreamdiary.web.entity.admin.TmplatDefEntity;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TmplatDefSpec
-        implements BaseSpec<TmplatDefEntity> {
+        implements BaseCrudSpec<TmplatDefEntity>,
+                   BaseStateSpec<TmplatDefEntity> {
 
     //
 }
--- src/main/java/io/nicheblog/dreamdiary/web/spec/admin/TmplatTxtSpec.java
+++ src/main/java/io/nicheblog/dreamdiary/web/spec/admin/TmplatTxtSpec.java
@@ -1,6 +1,7 @@
 package io.nicheblog.dreamdiary.web.spec.admin;
 
-import io.nicheblog.dreamdiary.global.intrfc.spec.BaseSpec;
+import io.nicheblog.dreamdiary.global.intrfc.spec.BaseCrudSpec;
+import io.nicheblog.dreamdiary.global.intrfc.spec.embed.BaseStateSpec;
 import io.nicheblog.dreamdiary.web.entity.admin.TmplatTxtEntity;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +14,10 @@ import org.springframework.stereotype.Component;
  * @author nichefish
  * @implements BaseSpec:: 세부내용 변경시 해당 default 메소드 재정의(@Override)
  */
-@Component("tmplatTxtSpec")
+@Component
 public class TmplatTxtSpec
-        implements BaseSpec<TmplatTxtEntity> {
+        implements BaseCrudSpec<TmplatTxtEntity>,
+                   BaseStateSpec<TmplatTxtEntity> {
 
     //
 }
