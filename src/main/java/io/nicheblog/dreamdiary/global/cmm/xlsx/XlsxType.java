5370534a2bbdbf0c32a51efe0381bc84945a31f7
nichefish <nichefish@gmail.com>
2024-04-08 00:58:08 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/global/cmm/xlsx/XlsxType.java
@@ -0,0 +1,29 @@
+package io.nicheblog.dreamdiary.global.cmm.xlsx;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * ContentType
+ * <pre>
+ *  ClsfEntity를 상속받은 클래스들이 사용하는 컨텐츠 타입 정보
+ * </pre>
+ *
+ * @author nichefish
+ */
+@RequiredArgsConstructor
+public enum XlsxType {
+
+    NOTICE(
+            "공지사항",
+            "공지사항 #1",
+            "공지사항",
+            XlsxConstant.NOTICE
+    );
+
+    public final String fileNm;
+    public final String sheetNm;
+    public final String title;
+    public final List<XlsxCellDef> headers;
+}
