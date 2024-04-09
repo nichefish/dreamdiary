4f7ae16a08ca59e64589524fe253b413bd15029d
nichefish <nichefish@gmail.com>
2024-04-09 15:37:05 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/global/cmm/mail/handler/MailWorker.java
+++ src/main/java/io/nicheblog/dreamdiary/global/cmm/mail/handler/MailWorker.java
@@ -1,14 +1,21 @@
 package io.nicheblog.dreamdiary.global.cmm.mail.handler;
 
+import io.nicheblog.dreamdiary.global.cmm.log.ActvtyCtgr;
+import io.nicheblog.dreamdiary.global.cmm.log.event.LogSysEvent;
+import io.nicheblog.dreamdiary.global.cmm.log.model.LogSysParam;
 import io.nicheblog.dreamdiary.global.cmm.mail.event.MailSendEvent;
 import io.nicheblog.dreamdiary.global.cmm.mail.model.MailSendParam;
 import io.nicheblog.dreamdiary.global.cmm.mail.service.MailService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * MailWorker
@@ -24,39 +31,44 @@ public class MailWorker implements Runnable {
 
 	@Resource(name = "mailService")
 	private MailService mailService;
+	@Resource
+	protected ApplicationEventPublisher publisher;
 
 	/** 메일 queue */
-	private Queue<MailSendEvent> mailQueue = new ConcurrentLinkedQueue<>();
+	private static final BlockingQueue<MailSendEvent> mailQueue = new LinkedBlockingQueue<>();
+
+	@PostConstruct
+	public void init() {
+		Thread workerThread = new Thread(this);
+		workerThread.start();
+	}
 
 	@Override
 	public void run() {
 		while (true) {
-			consume();
+			// Blocks until an element is available
 			try {
-				synchronized (mailQueue) {
-					mailQueue.wait();
-				}
-			} catch (InterruptedException ex) {
-				log.error(ex);
-			}
-		}
-	}
-
-	/** 메일 발송 처리 */
-	private void consume() {
-		while (!mailQueue.isEmpty()) {
-			MailSendEvent event = mailQueue.poll();
-			MailSendParam mailSendParam = event.getMailSendParam();
-			
-			try {
+				// 메일 발송 처리
+				MailSendEvent mailEvent = mailQueue.take();
+				MailSendParam mailSendParam = mailEvent.getMailSendParam();
 				mailService.send(mailSendParam);
+			} catch (InterruptedException e) {
+				log.warn("mail send failed", e);
+				Thread.currentThread().interrupt();
+				LogSysParam logParam = new LogSysParam(true, "메일 발송에 실패했습니다.", ActvtyCtgr.SYSTEM);
+				logParam.setExceptionInfo(e);
+				publisher.publishEvent(new LogSysEvent(this, logParam));
 			} catch (Exception e) {
-				throw new RuntimeException("메일 발송 중 에러가 발생했습니다.", e);
+				log.warn("mail send failed", e);
+				LogSysParam logParam = new LogSysParam(true, "메일 발송에 실패했습니다.", ActvtyCtgr.SYSTEM);
+				logParam.setExceptionInfo(e);
+				publisher.publishEvent(new LogSysEvent(this, logParam));
 			}
 		}
 	}
 
 	public void offer(MailSendEvent event) {
-		this.mailQueue.offer(event);
+		boolean isSuccess = mailQueue.offer(event);
+		if (!isSuccess) log.warn("queue offer failed... {}", event.toString());
 	}
 }
