6fa4e3a4033fc25614c5c2fa470f4a130a2c08bb
nichefish <nichefish@gmail.com>
2024-04-12 08:29:56 +0900

    -

---
+++ src/test/java/io/nicheblog/dreamdiary/web/mapstruct/user/reqst/UserReqstMapstructTest.java
@@ -0,0 +1,26 @@
+package io.nicheblog.dreamdiary.web.mapstruct.user.reqst;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * UserReqstMapstructTest
+ * <pre>
+ *  사용자 계정 신청 Mapstruct 테스트 모듈
+ * </pre>
+ *
+ * @author nichefish
+ */
+class UserReqstMapstructTest {
+    
+    @Test
+    void toDto() {
+
+    }
+
+    @Test
+    void toEntity() {
+
+    }
+}
