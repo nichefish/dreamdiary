b2fa7f6527fe33c9ba7e641222ed50990cc1725f
nichefish <nichefish@gmail.com>
2024-04-21 17:26:35 +0900

    -

--- src/test/java/io/nicheblog/dreamdiary/web/mapstruct/vcatn/papr/VcatnPaprMapstructTest.java
+++ src/test/java/io/nicheblog/dreamdiary/web/mapstruct/vcatn/papr/VcatnPaprMapstructTest.java
@@ -3,8 +3,12 @@ package io.nicheblog.dreamdiary.web.mapstruct.vcatn.papr;
 import io.nicheblog.dreamdiary.web.entity.vcatn.papr.VcatnPaprEntity;
 import io.nicheblog.dreamdiary.web.model.vcatn.papr.VcatnPaprDto;
 import io.nicheblog.dreamdiary.web.model.vcatn.papr.VcatnPaprDtoTestFactory;
+import io.nicheblog.dreamdiary.web.model.vcatn.schdul.VcatnSchdulDto;
+import io.nicheblog.dreamdiary.web.model.vcatn.schdul.VcatnSchdulDtoTestFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -35,5 +39,26 @@ class VcatnPaprMapstructTest {
         // Then::
         // 일반 필드는 검증할 필요 없음. 로직이 들어가는 부분에 대하여 테스트 진행
         assertNotNull(vcatnPaprEntity);
+    }
+
+    /**
+     * toEntity 검증 :: 하위 엔티티
+     */
+    @Test
+    void toEntity_checkSchdulList() throws Exception {
+        // Given::
+        VcatnPaprDto.DTL vcatnPaprDto = VcatnPaprDtoTestFactory.createVcatnPaprDtlDto();
+        // 휴가 일정 세팅
+        VcatnSchdulDto aa  = VcatnSchdulDtoTestFactory.createVcatnSchdulDtlDto();
+        VcatnSchdulDto bb  = VcatnSchdulDtoTestFactory.createVcatnSchdulDtlDto();
+        vcatnPaprDto.setSchdulList(List.of(aa, bb));
+
+        // When::
+        VcatnPaprEntity vcatnPaprEntity = vcatnPaprMapstruct.toEntity(vcatnPaprDto);
+
+        // Then::
+        // 일반 필드는 검증할 필요 없음. 로직이 들어가는 부분에 대하여 테스트 진행
+        assertNotNull(vcatnPaprEntity);
+        assertNotNull(vcatnPaprEntity.getSchdulList());
     }
 }
