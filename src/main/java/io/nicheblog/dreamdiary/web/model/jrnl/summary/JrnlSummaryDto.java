ccbfb764597d6eeebd6ffedb3bdad285ea8c0b1a
nichefish <nichefish@gmail.com>
2024-04-21 18:21:59 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/web/model/jrnl/summary/JrnlSummaryDto.java
@@ -0,0 +1,58 @@
+package io.nicheblog.dreamdiary.web.model.jrnl.summary;
+
+import io.nicheblog.dreamdiary.global.ContentType;
+import io.nicheblog.dreamdiary.global.intrfc.model.BasePostDto;
+import io.nicheblog.dreamdiary.global.intrfc.model.Identifiable;
+import io.nicheblog.dreamdiary.global.intrfc.model.cmpstn.CommentCmpstn;
+import io.nicheblog.dreamdiary.global.intrfc.model.cmpstn.CommentCmpstnModule;
+import io.nicheblog.dreamdiary.global.intrfc.model.cmpstn.TagCmpstn;
+import io.nicheblog.dreamdiary.global.intrfc.model.cmpstn.TagCmpstnModule;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * JrnlSummaryDto
+ * <pre>
+ *  저널 결산 Dto.
+ * </pre>
+ *
+ * @author nichefish
+ * @extends BasePostDto
+ * @implements CommentCmpstnModule, TagCmpstnModule
+ */
+@Getter
+@Setter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+public class JrnlSummaryDto
+        extends BasePostDto
+        implements Identifiable<Integer>, CommentCmpstnModule, TagCmpstnModule {
+
+    /** 필수: 컨텐츠 타입 */
+    private static final ContentType CONTENT_TYPE = ContentType.JRNL_DIARY;
+    /** 필수(Override): 글분류 코드 */
+    private static final String CTGR_CL_CD = CONTENT_TYPE.name() + "_CTGR_CD";
+
+    /** 컨텐츠 타입 */
+    @Builder.Default
+    protected String contentType = CONTENT_TYPE.key;
+
+    /* ----- */
+
+    /** 결산 년도 */
+    private Integer yy;
+
+    @Override
+    public Integer getKey() {
+        return this.postNo;
+    }
+
+    /* ----- */
+
+    /** 댓글 정보 모듈 (위임) */
+    public CommentCmpstn comment;
+    /** 태그 정보 모듈 (위임) */
+    public TagCmpstn tag;
+}
