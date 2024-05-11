83720d3c295c1d5b8186a1ebfcf8ba9cc5718cb7
nichefish <nichefish@gmail.com>
2024-05-11 19:06:13 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/service/jrnl/sumry/JrnlSumryCnService.java
+++ src/main/java/io/nicheblog/dreamdiary/web/service/jrnl/sumry/JrnlSumryCnService.java
@@ -1,14 +1,14 @@
 package io.nicheblog.dreamdiary.web.service.jrnl.sumry;
 
 import io.nicheblog.dreamdiary.global.intrfc.service.BaseMultiCrudService;
+import io.nicheblog.dreamdiary.global.util.EhCacheUtils;
 import io.nicheblog.dreamdiary.web.entity.jrnl.sumry.JrnlSumryCnEntity;
 import io.nicheblog.dreamdiary.web.mapstruct.jrnl.sumry.JrnlSumryCnMapstruct;
 import io.nicheblog.dreamdiary.web.model.jrnl.sumry.JrnlSumryCnDto;
 import io.nicheblog.dreamdiary.web.repository.jrnl.sumry.JrnlSumryCnRepository;
 import io.nicheblog.dreamdiary.web.spec.jrnl.sumry.JrnlSumryCnSpec;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Caching;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -48,7 +48,7 @@ public class JrnlSumryCnService
     }
 
     /**
-     * 등록 전처리
+     * 등록 전처리 :: override
      */
     @Override
     public void preRegist(final JrnlSumryCnDto jrnlSumryCn) {
@@ -59,30 +59,47 @@ public class JrnlSumryCnService
         jrnlSumryCn.setIdx(lastIndex + 1);
     }
 
+    /**
+     * 등록 후처리 :: override
+     */
     @Override
-    @Caching(evict = {
-            @CacheEvict(value="jrnlSumryDtl", key="#rslt.getJrnlSumryNo()"),
-            @CacheEvict(value="jrnlSumryDtlByYy", key="#rslt.getYy()")
-    })
     public void postRegist(final JrnlSumryCnEntity rslt) throws Exception {
-        //
+        // jrnl_sumry
+        EhCacheUtils.evictCache("jrnlSumryDtl", rslt.getJrnlSumryNo().toString());
+        EhCacheUtils.evictCache("jrnlSumryDtlByYy", rslt.getJrnlSumry().getYy().toString());
+        // jrnl_sumry_cn
+        EhCacheUtils.evictCache("jrnlSumryCnDtlDto", rslt.getPostNo().toString());
     }
 
+    /**
+     * 상세 조회 (dto level) :: 캐시 처리
+     */
+    @Cacheable(value="jrnlSumryCnDtlDto", key="#key")
+    public JrnlSumryCnDto getDtlDtoWithCache(Integer key) throws Exception {
+        return this.getDtlDto(key);
+    }
+
+    /**
+     * 수정 후처리 :: override
+     */
     @Override
-    @Caching(evict = {
-            @CacheEvict(value="jrnlSumryDtl", key="#rslt.getJrnlSumryNo()"),
-            @CacheEvict(value="jrnlSumryDtlByYy", key="#rslt.getYy()")
-    })
     public void postModify(final JrnlSumryCnEntity rslt) throws Exception {
-        //
+        // jrnl_sumry
+        EhCacheUtils.evictCache("jrnlSumryDtl", rslt.getJrnlSumryNo().toString());
+        EhCacheUtils.evictCache("jrnlSumryDtlByYy", rslt.getJrnlSumry().getYy().toString());
+        // jrnl_sumry_cn
+        EhCacheUtils.evictCache("jrnlSumryCnDtlDto", rslt.getPostNo().toString());
     }
 
+    /**
+     * 삭제 후처리 :: override
+     */
     @Override
-    @Caching(evict = {
-            @CacheEvict(value="jrnlSumryDtl", key="#rslt.getJrnlSumryNo()"),
-            @CacheEvict(value="jrnlSumryDtlByYy", key="#rslt.getYy()")
-    })
     public void postDelete(final JrnlSumryCnEntity rslt) throws Exception {
-        //
+        // jrnl_sumry
+        EhCacheUtils.evictCache("jrnlSumryDtl", rslt.getJrnlSumryNo().toString());
+        EhCacheUtils.evictCache("jrnlSumryDtlByYy", rslt.getJrnlSumry().getYy().toString());
+        // jrnl_sumry_cn
+        EhCacheUtils.evictCache("jrnlSumryCnDtlDto", rslt.getPostNo().toString());
     }
 }
