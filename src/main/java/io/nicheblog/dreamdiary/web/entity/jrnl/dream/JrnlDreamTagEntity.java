e7ab20e411cf6440e5f9b411ccebf225bbcdff86
nichefish <nichefish@gmail.com>
2024-05-13 02:58:24 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/entity/jrnl/dream/JrnlDreamTagEntity.java
+++ src/main/java/io/nicheblog/dreamdiary/web/entity/jrnl/dream/JrnlDreamTagEntity.java
@@ -1,7 +1,6 @@
 package io.nicheblog.dreamdiary.web.entity.jrnl.dream;
 
 import io.nicheblog.dreamdiary.global.intrfc.entity.BaseCrudEntity;
-import io.nicheblog.dreamdiary.web.entity.cmm.tag.TagSmpEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,73 +8,56 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.*;
 
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.*;
+import java.util.List;
 
 /**
  * JrnlDreamTagEntity
  * <pre>
  *  저널 꿈 태그 Entity
- *  (사용 용이성을 위해 엔티티 분리)
  * </pre>
  *
  * @author nichefish
  * @extends BaseCrudEntity
  */
 @Entity
-@Table(name = "content_tag")
+@Table(name = "tag")
 @Getter
 @Setter
 @SuperBuilder(toBuilder = true)
 @RequiredArgsConstructor
 @AllArgsConstructor
-@Where(clause = "ref_content_type='JRNL_DREAM' AND del_yn='N'")
-@SQLDelete(sql = "UPDATE content_tag SET del_yn = 'Y' WHERE content_tag_no = ?")
+@Where(clause = "del_yn='N'")
+@SQLDelete(sql = "UPDATE tag SET del_yn = 'Y' WHERE tag_no = ?")
 public class JrnlDreamTagEntity
         extends BaseCrudEntity {
 
-    @PostLoad
-    private void onLoad() {
-        // 태그 이름 세팅
-        if (this.tag != null) this.tagNm = this.tag.getTagNm();
-    }
-
-    /** 컨텐츠 태그 번호 (PK) */
+    /** 태그 번호 (PK) */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "content_tag_no")
-    @Comment("컨텐츠 태그 번호 (PK)")
-    private Integer contentTagNo;
+    @Column(name = "tag_no")
+    @Comment("태그 번호 (PK)")
+    private Integer tagNo;
 
-    /** 참조 태그 번호 */
-    @Column(name = "ref_tag_no")
-    @Comment("참조 태그 번호")
-    private Integer refTagNo;
-
-    /** 참조 글 번호 */
-    @Column(name = "ref_post_no")
-    @Comment("참조 글 번호")
-    private Integer refPostNo;
-
-    /** 참조 컨텐츠 타입 */
-    @Column(name = "ref_content_type")
-    @Comment("참조 컨텐츠 타입")
-    private String refContentType;
-
-    /** 태그 정보 */
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "ref_tag_no", referencedColumnName = "tag_no", updatable = false, insertable = false)
-    @NotFound(action = NotFoundAction.IGNORE)
-    private TagSmpEntity tag;
-
-    @Transient
+    /** 태그 */
+    @Column(name = "tag_nm")
+    @Comment("태그")
     private String tagNm;
 
-    /** 참조 컨텐츠 (저널 꿈)  */
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "ref_post_no", referencedColumnName = "post_no", insertable = false, updatable = false)
+    /** 저널 꿈 태그 */
+    @OneToMany(mappedBy = "tag", fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
     @NotFound(action = NotFoundAction.IGNORE)
-    @Comment("저널 꿈 정보")
-    private JrnlDreamEntity jrnlDream;
+    private List<JrnlDreamContentTagEntity> jrnlDreamTagList;
+
+    /* ----- */
+
+    /**
+     * 생성자
+     */
+    public JrnlDreamTagEntity(final String tagNm) {
+        this.tagNm = tagNm;
+    }
 }
