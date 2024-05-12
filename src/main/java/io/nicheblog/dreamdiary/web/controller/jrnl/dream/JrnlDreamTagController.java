143469f5c931bbc1474cb920fd9ee1b8123ef88e
nichefish <nichefish@gmail.com>
2024-05-13 02:51:28 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/controller/jrnl/dream/JrnlDreamTagController.java
+++ src/main/java/io/nicheblog/dreamdiary/web/controller/jrnl/dream/JrnlDreamTagController.java
@@ -19,7 +19,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.Collections;
