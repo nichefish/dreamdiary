e9df94896d3c0e49100df4e09b4530a4b5de6620
nichefish <nichefish@gmail.com>
2024-07-31 13:00:48 +0900

    -

---
+++ src/main/java/io/nicheblog/dreamdiary/global/intrfc/entity/embed/SectnEmbedModule.java
@@ -0,0 +1,17 @@
+package io.nicheblog.dreamdiary.global.intrfc.entity.embed;
+
+/**
+ * SectnEmbedModule
+ * <pre>
+ *   단락 모듈 인터페이스
+ * </pre>
+ *
+ * @author nichefish
+ */
+public interface SectnEmbedModule {
+    /** Getter */
+    SectnEmbed getSectn();
+
+    /** Setter */
+    void setSectn(SectnEmbed embed);
+}
---
+++ src/main/java/io/nicheblog/dreamdiary/global/intrfc/model/cmpstn/SectnCmpstn.java
@@ -0,0 +1,33 @@
+package io.nicheblog.dreamdiary.global.intrfc.model.cmpstn;
+
+import io.nicheblog.dreamdiary.web.model.cmm.cn.SectnDto;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * SectnCmpstn
+ * <pre>
+ *  단락 관련 정보 위임
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SectnCmpstn
+        implements Serializable {
+
+    /** 단락 목록 */
+    private List<SectnDto> list;
+    /** 단락 갯수 */
+    @Builder.Default
+    private Integer cnt = 0;
+    /** 단락 존재 여부 */
+    @Builder.Default
+    private Boolean hasSectn = false;
+}
