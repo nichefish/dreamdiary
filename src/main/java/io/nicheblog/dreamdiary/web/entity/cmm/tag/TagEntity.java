02f2454436053ab82e8e2fbd5e167804206327a0
nichefish <nichefish@gmail.com>
2024-08-16 22:13:47 +0900

    -

--- src/main/java/io/nicheblog/dreamdiary/web/entity/cmm/tag/TagEntity.java
+++ src/main/java/io/nicheblog/dreamdiary/web/entity/cmm/tag/TagEntity.java
@@ -54,7 +54,7 @@ public class TagEntity
     /** 컨텐츠 태그 */
     @OneToMany(mappedBy = "tag", fetch = FetchType.EAGER)
     @Fetch(FetchMode.SELECT)
-    @Where(clause = "ref_content_type not in ('JRNL_DREAM', 'JRNL_DIARY')")
+    @Where(clause = "ref_content_type not in ('JRNL_DAY', 'JRNL_DIARY', 'JRNL_DREAM')")
     @NotFound(action = NotFoundAction.IGNORE)
     private List<ContentTagEntity> contentTagList;
 
