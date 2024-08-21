ceab4c9597571b42d3491fd5e137a42d3b558297
nichefish <nichefish@gmail.com>
2024-08-22 04:49:26 +0900

    redis 관련 설정 추가

---
+++ src/main/java/io/nicheblog/dreamdiary/global/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package io.nicheblog.dreamdiary.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+
+/**
+ * RedisConfig
+ * <pre>
+ *  Redis 관련 설정 커스터마이즈
+ * </pre>
+ *
+ * @author nichefish
+ */
+@Configuration
+public class RedisConfig {
+
+    @Resource(name = "redisProperty")
+    private RedisProperty redisProperty;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperty.getHost());
+        redisStandaloneConfiguration.setPort(redisProperty.getPort());
+        redisStandaloneConfiguration.setPassword(redisProperty.getPassword());
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
