054dc56bc81a98a2a5f6a6b9ada25b0530e5df5b
nichefish <nichefish@gmail.com>
2024-08-24 04:13:04 +0900

    kafka 관련 초기 설정 및 테스트 완료

---
+++ src/main/java/io/nicheblog/dreamdiary/api/kafka/KafkaApiController.java
@@ -0,0 +1,33 @@
+package io.nicheblog.dreamdiary.api.kafka;
+
+import io.nicheblog.dreamdiary.global.util.KafkaUtils;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * KafkaApiController
+ * <pre>
+ *  Kafka 관련 테스트 및 수동 처리 컨트롤러.
+ * </pre>
+ *
+ * @author nichefish
+ */
+@RestController
+@Log4j2
+public class KafkaApiController {
+
+    @RequestMapping(value = "/kafka")
+    @ResponseBody
+    public String sendMessage(
+            @RequestParam String message
+    ) {
+
+        log.info("message : {}", message);
+        KafkaUtils.sendMessage(message);
+
+        return "success";
+    }
+}
