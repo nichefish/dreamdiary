aa573bb07ddb96cc895bd44557cf929833cc14dd
nichefish <nichefish@gmail.com>
2024-08-23 22:27:18 +0900

    redis와 ehcache 동시에 사용 가능하도록 설정 추가 (ehcache 우선, 없으면 redis)

--- src/main/java/io/nicheblog/dreamdiary/global/auth/entity/RefreshToken.java
+++ src/main/java/io/nicheblog/dreamdiary/global/auth/entity/RefreshToken.java
@@ -1,11 +1,11 @@
 package io.nicheblog.dreamdiary.global.auth.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 
 /**
  * RefreshToken
--- src/main/java/io/nicheblog/dreamdiary/global/config/CacheConfig.java
+++ src/main/java/io/nicheblog/dreamdiary/global/config/CacheConfig.java
@@ -1,26 +1,38 @@
 package io.nicheblog.dreamdiary.global.config;
 
 import lombok.SneakyThrows;
+import org.ehcache.xml.XmlConfiguration;
+import org.ehcache.jsr107.EhcacheCachingProvider;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
+import javax.cache.Caching;
+import java.net.URL;
 
 /**
- * RedisConfig
+ * CacheConfig
  * <pre>
- *  Redis 관련 설정 커스터마이즈
+ *  Cache 관련 설정 커스터마이즈
  * </pre>
  *
  * @author nichefish
  */
 @Configuration
-public class RedisConfig {
+public class CacheConfig {
 
     @Resource(name = "redisProperty")
     private RedisProperty redisProperty;
@@ -49,5 +61,36 @@ public class RedisConfig {
         // 모든 경우
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Primary
+    @Bean(name = "cacheManager")
+    public CacheManager redisCacheManager() {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        RedisSerializer.json()));
+
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.builder(redisConnectionFactory())
+                .cacheDefaults(cacheConfiguration);
+
+        return builder.build();
+    }
+
+    @Bean
+    public JCacheCacheManager jCacheCacheManager() {
+        return new JCacheCacheManager(ehCacheManager());
+    }
+
+    @Bean
+    public javax.cache.CacheManager ehCacheManager() {
+        URL myUrl = getClass().getClassLoader().getResource("ehcache.xml");
+        org.ehcache.config.Configuration xmlConfig = new XmlConfiguration(myUrl);
+        EhcacheCachingProvider provider = (EhcacheCachingProvider) Caching.getCachingProvider("org.ehcache.jsr107.EhcacheCachingProvider");
+        return provider.getCacheManager(provider.getDefaultURI(),  xmlConfig);
+    }
+
+    @Bean
+    public CacheResolver cacheResolver() {
+        return new CustomCacheResolver(jCacheCacheManager(), redisCacheManager());
     }
 }
