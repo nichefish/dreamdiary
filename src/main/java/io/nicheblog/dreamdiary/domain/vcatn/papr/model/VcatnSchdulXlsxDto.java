abb94f4e7870a40ac5ea1429b102df2657e9703c
nichefish <nichefish@gmail.com>
2024-10-10 00:10:36 +0900

    1. 패키지 구조 대규모 개편. (레이어 기반 -> 도메인 기반.) 2. javadoc 작성 중.

--- src/main/java/io/nicheblog/dreamdiary/domain/vcatn/papr/model/VcatnSchdulXlsxDto.java
+++ src/main/java/io/nicheblog/dreamdiary/domain/vcatn/papr/model/VcatnSchdulXlsxDto.java
@@ -1,11 +1,11 @@
-package io.nicheblog.dreamdiary.web.model.vcatn.schdul;
+package io.nicheblog.dreamdiary.domain.vcatn.papr.model;
 
 import lombok.*;
 
 /**
  * VcatnSchdulXlsxDto
  * <pre>
- *  휴가일자 엑셀 다운로드 Dto
+ *  휴가일자 엑셀 다운로드 Dto.
  * </pre>
  *
  * @author nichefish
@@ -13,28 +13,22 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode
 @ToString
 public class VcatnSchdulXlsxDto {
 
-    /**
-     * 날짜
-     */
+    /** 날짜 */
     private String vcatnSchdul;
-    /**
-     * 사용자
-     */
+
+    /** 사용자 */
     private String userNm;
-    /**
-     * 일정 분류 이름
-     */
+
+    /** 일정 분류 이름 */
     private String vcatnNm;
-    /**
-     * 소진일
-     */
+
+    /** 소진일 */
     private String vcatnExprDy;
-    /**
-     * 일정 비고
-     */
+
+    /** 일정 비고 */
     private String vcatnRm;
 }
--- src/main/java/io/nicheblog/dreamdiary/domain/vcatn/stats/model/VcatnStatsDto.java
+++ src/main/java/io/nicheblog/dreamdiary/domain/vcatn/stats/model/VcatnStatsDto.java
@@ -1,12 +1,12 @@
-package io.nicheblog.dreamdiary.web.model.vcatn.stats;
+package io.nicheblog.dreamdiary.domain.vcatn.stats.model;
 
-import io.nicheblog.dreamdiary.web.model.user.UserDto;
+import io.nicheblog.dreamdiary.domain.user.info.model.UserDto;
 import lombok.*;
 
 /**
  * VcatnStatsDto
  * <pre>
- *  휴가계획서 통계 Dto
+ *  휴가계획서 통계 Dto.
  * </pre>
  *
  * @author nichefish
@@ -16,40 +16,50 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode
 @ToString
 public class VcatnStatsDto {
 
     /** userId */
     private String userId;
+
     /** User */
     private UserDto.LIST user;
+
     /** 산정기준년도 */
     private String statsYy;
+
     /** 산정기준년도 */
     private String statsDt;
 
     /** 근속년수 */
     @Builder.Default
     private Integer cnwkYy = 0;
+
     /** 기본연차 */
     @Builder.Default
     private Integer bsYryc = 0;
+
     /** 신입 여부 */
     @Builder.Default
     private Boolean isNewbie = false;
+
     /** 신입연차 */
     @Builder.Default
     private Integer newbieYryc = 0;
+
     /** 근속 추가연차 */
     @Builder.Default
     private Integer cnwkYryc = 0;
+
     /** 프로젝트 추가연차 */
     @Builder.Default
     private Integer prjctYryc = 0;
+
     /** 안식주 */
     @Builder.Default
     private Integer refreshYryc = 0;
+
 
     /** 총 연차 (프리마커 오류땜에 프로퍼티 추가) */
     @Builder.Default
@@ -62,20 +72,27 @@ public class VcatnStatsDto {
     /* ----- */
 
     /**
-     * 총 연차 (Getter overrided)
+     * Getter :: 총 연차
+     * @return double - 계산된 총 연차
      */
     public double getTotYryc() {
         return (this.bsYryc + this.newbieYryc + this.cnwkYryc + this.prjctYryc + this.refreshYryc);
     }
 
     /**
-     * 잔여 연차 (소진연차를 입력받거나 or 자체 정보로 계산하거나)
+     * Getter :: 잔여 연차 (소진연차를 입력받음)
+     * @param exhsYryc - 소진된 연차
+     * @return double - 계산된 잔여 연차
      */
     public double getRemndrYryc(final Double exhsYryc) {
         this.exhsYryc = exhsYryc;
         return this.getRemndrYryc();
     }
 
+    /**
+     * Getter :: 잔여 연차 (소진연차를 입력받지 않고 자체 정보로 계산)
+     * @return double - 계산된 잔여 연차
+     */
     public double getRemndrYryc() {
         return (this.getTotYryc() - this.exhsYryc);
     }
